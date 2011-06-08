8286b47e3016b0c124ac71ea6046a400ce76ff48
Mike Lohmann <mike.h.lohmann@googlemail.com>
2011-06-08 15:21:02 +0200

    Removed TestCase/Exception, because XPath's got its own.

--- src/LiveTest/TestCase/General/Html/Dom/XPath/Size.php
+++ src/LiveTest/TestCase/General/Html/Dom/XPath/Size.php
@@ -11,7 +11,6 @@ namespace LiveTest\TestCase\General\Html\Dom\XPath;
 
 use LiveTest\ConfigurationException;
 
-use LiveTest\TestCase\Exception;
 use DOMXPath;
 
 /**
@@ -21,41 +20,41 @@ use DOMXPath;
  */
 class Size extends TestCase
 {
-	private $xpath;
-	private $minSize;
-	private $maxSize;
+  private $xpath;
+  private $minSize;
+  private $maxSize;
 
-	/**
-	 * Sets the xpath, minSize, maxSize. At least one of the both sizes has to set.
-	 *
-	 * @param string $xpath
-	 * @param int $minSize
-	 * @param int $maxSize
-	 * @throws ConfigurationException
-	 */
-	public function init($xpath, $minSize = null, $maxSize = null)
-	{
-		if(is_null($minSize) && is_null($maxSize))
-		{
-			throw new ConfigurationException('Neither minSoize nor maxSize is set.');
-		}
+  /**
+   * Sets the xpath, minSize, maxSize. At least one of the both sizes has to set.
+   *
+   * @param string $xpath
+   * @param int $minSize
+   * @param int $maxSize
+   * @throws ConfigurationException
+   */
+  public function init($xpath, $minSize = null, $maxSize = null)
+  {
+    if(is_null($minSize) && is_null($maxSize))
+    {
+      throw new ConfigurationException('Neither minSoize nor maxSize is set.');
+    }
 
-		$this->minSize = $minSize;
-		$this->maxSize = $maxSize;
+    $this->minSize = $minSize;
+    $this->maxSize = $maxSize;
 
-		$this->xpath = $xpath;
-	}
+    $this->xpath = $xpath;
+  }
 
-	/**
-	 * Checks if the elements that a specified by a xpath have a valid size.
-	 *
-	 * @see LiveTest\TestCase\General\Html\Dom\XPath.TestCase::doXPathTest()
-	 *
-	 * @param DOMXPath $domXPath
-	 */
-	public function doXPathTest(DOMXPath $domXPath)
-	{
-		$elements = $domXPath->query($this->xpath);
+  /**
+   * Checks if the elements that a specified by a xpath have a valid size.
+   *
+   * @see LiveTest\TestCase\General\Html\Dom\XPath.TestCase::doXPathTest()
+   *
+   * @param DOMXPath $domXPath
+   */
+  public function doXPathTest(DOMXPath $domXPath)
+  {
+    $elements = $domXPath->query($this->xpath);
     if ($elements->length == 0)
     {
       throw new Exception('The given xpath ("' . $this->xpath . '") was not found.');
@@ -73,17 +72,17 @@ class Size extends TestCase
           $value = $element->textContent;
       }
 
-			$size = strlen($value);
+      $size = strlen($value);
 
       if( !is_null( $this->maxSize ) && $this->maxSize < $size )
       {
-      	throw new Exception('The size of the xpath element ("'.$this->xpath.'") is too big (current: '.$size.', max: '.$this->maxSize.').');
+        throw new Exception('The size of the xpath element ("'.$this->xpath.'") is too big (current: '.$size.', max: '.$this->maxSize.').');
       }
 
       if( !is_null( $this->minSize ) && $this->minSize > $size )
       {
-      	throw new Exception('The size of the xpath element ("'.$this->xpath.'") is too small (current: '.$size.', min: '.$this->minSize.').');
+        throw new Exception('The size of the xpath element ("'.$this->xpath.'") is too small (current: '.$size.', min: '.$this->minSize.').');
       }
     }
-	}
+  }
 }
